71004cbb7f7efc9ad8899f2474809a23915ccee3
stoppler <ry@ryans-mbp.hitronhub.home>
2017-01-15 15:39:17 -0800

    added formatted output to Student class

--- Student.php
+++ Student.php
@@ -31,4 +31,22 @@ class Student {
         $this->grades[] = $grade;
     }
     
+    //calculates and returns average grade
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    
+    //formats print statement
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
+    }
+    
 }
